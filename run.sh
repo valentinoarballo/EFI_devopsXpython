93e4af6983a219c87ba9dd2557636e51561b7df6
valentino <v.arballo@itecriocuarto.org.ar>
2023-10-26 17:03:06 -0300

    run.sh fixed

--- run.sh
+++ run.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+
+echo "RUNNING > db init"
 flask db init
+sleep 10
+
+echo "RUNNING > db migrate -m initial_migration"
 flask db migrate -m "initial_migration"
+sleep 10
+
+echo "RUNNING > db upgrade"
 flask db upgrade
-gunicorn app:app --bind 0.0.0.0:5055 
+sleep 10
+
+gunicorn app:app --bind 0.0.0.0:5055 --reload
